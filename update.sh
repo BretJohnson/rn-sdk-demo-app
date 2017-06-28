ef825a48ac34a4a6438c3fe517b5d9ff2c4dbb52
Bret Johnson <bret.johnson@microsoft.com>
2017-06-28 02:17:38 -0400

    Added mobile-center to the update.sh script & switched to single react-native link

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ rm -rf node_modules
 yarn install
 
 echo 'Installing our npm packages...'
+yarn add mobile-center
 yarn add mobile-center-analytics
 yarn add mobile-center-crashes
 yarn add mobile-center-push
@@ -20,6 +21,4 @@ rm ios/Podfile.lock
 echo 'Linking...'
 echo 'Android app secret should be: 13502f6c-53bc-496e-9517-f2454b4e03a5'
 echo 'iOS app secret should be: 96638174-9d13-4b2b-a110-1866d6bfa7dc'
-react-native link mobile-center-analytics
-react-native link mobile-center-crashes
-react-native link mobile-center-push
+react-native link
